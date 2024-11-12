4c1c7bc584319ab5570429ef795d11ebe0ac46d1
20210770 <punnlert@kaist.ac.kr>
2024-11-13 00:31:05 +0900

    remove build from main

--- build/_app/immutable/chunks/3.a9c5c377.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/(waves)-layout.svelte.1ce4f87f.js";export{t as component};
--- build/_app/immutable/chunks/8.f037987e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/(waves)-404-page.svelte.29315359.js";export{t as component};
